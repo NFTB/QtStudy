0e863aa136a93c7b2f12a63bc49ed0b9d331180c
YANGMENGJIE <2757629107@qq.com>
2024-08-15 16:29:00 +0800

    impl palette

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,7 +6,28 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    ui->redSlider->setRange(0,255);
+    ui->greenSlider->setRange(0,255);
+    ui->blueSlider->setRange(0,255);
+    ui->alphaSlider->setRange(0,255);
+    ui->alphaSlider->setTickPosition(QSlider::TicksBelow);
+    connect(ui->redSlider, &QSlider::valueChanged, this, &MainWindow::updateTextColor);
+    connect(ui->greenSlider, &QSlider::valueChanged, this, &MainWindow::updateTextColor);
+    connect(ui->blueSlider, &QSlider::valueChanged, this, &MainWindow::updateTextColor);
+    connect(ui->alphaSlider, &QSlider::valueChanged, this, &MainWindow::updateTextColor);
 }
+
+void MainWindow::updateTextColor(){
+    int red = ui->redSlider->value();
+    int green = ui->greenSlider->value();
+    int blue = ui->blueSlider->value();
+    int alpha = ui->alphaSlider->value();
+    QColor color(red, green, blue, alpha);
+    QPalette p = ui->colorTextEdit->palette();
+    p.setColor(QPalette::Base, color);
+    ui->colorTextEdit->setPalette(p);
+}
+
 
 MainWindow::~MainWindow()
 {
--- mainwindow.h
+++ mainwindow.h
@@ -19,5 +19,6 @@ public:
 
 private:
     Ui::MainWindow *ui;
+    void updateTextColor();
 };
 #endif // MAINWINDOW_H
