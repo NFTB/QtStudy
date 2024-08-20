a3f40dbd988ffb95152242238466ea82e97df04b
YANGMENGJIE <2757629107@qq.com>
2024-08-20 11:08:02 +0800

    new ui graphic design

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -9,6 +9,66 @@ MainWindow::MainWindow(QWidget *parent)
     setPalette(QPalette(Qt::white));
     paletteInit();
     timerInit();
+    ui->View->keyPress(nullptr);
+    // connect(ui->stackedWidget, &QStackedWidget::currentChanged, this, &MainWindow::onStackedWidgetPageChanged);
+    if (ui->stackedWidget->currentIndex() == 1) {
+        drawInit();
+    }
+}
+
+void MainWindow::drawInit(){
+    labViewCord=new QLabel("View 坐标：");
+    labViewCord->setMinimumWidth(150);
+    ui->statusBar->addWidget(labViewCord);
+
+    labSceneCord=new QLabel("Scene 坐标：");
+    labSceneCord->setMinimumWidth(150);
+    ui->statusBar->addWidget(labSceneCord);
+
+    labItemCord=new QLabel("Item 坐标：");
+    labItemCord->setMinimumWidth(150);
+    ui->statusBar->addWidget(labItemCord);
+
+    labItemInfo=new QLabel("ItemInfo: ");
+    labItemInfo->setMinimumWidth(200);
+    ui->statusBar->addWidget(labItemInfo);
+
+    scene=new QGraphicsScene(-300,-200,600,200);
+
+    ui->View->setScene(scene);
+
+    ui->View->setCursor(Qt::CrossCursor);
+    ui->View->setMouseTracking(true);
+    ui->View->setDragMode(QGraphicsView::RubberBandDrag);
+
+    this->setCentralWidget(ui->View);
+
+    connect(ui->View,SIGNAL(mouseMovePoint(QPoint)),
+                     this, SLOT(on_mouseMovePoint(QPoint)));
+
+    connect(ui->View,SIGNAL(mouseClicked(QPoint)),
+                     this, SLOT(on_mouseClicked(QPoint)));
+
+    connect(ui->View,SIGNAL(mouseDoubleClick(QPoint)),
+                     this, SLOT(on_mouseDoubleClick(QPoint)));
+
+    connect(ui->View,SIGNAL(keyPress(QKeyEvent*)),
+                     this, SLOT(on_keyPress(QKeyEvent*)));
+}
+
+void MainWindow::resetDrawPage(){
+    ui->statusBar->removeWidget(labViewCord);
+    ui->statusBar->removeWidget(labSceneCord);
+    ui->statusBar->removeWidget(labItemCord);
+    ui->statusBar->removeWidget(labItemInfo);
+    scene->clear();
+    ui->View->setCursor(Qt::ArrowCursor);
+    ui->View->setMouseTracking(false);
+    ui->View->setDragMode(QGraphicsView::NoDrag);
+    disconnect(ui->View, SIGNAL(mouseMovePoint(QPoint)), this, SLOT(on_mouseMovePoint(QPoint)));
+    disconnect(ui->View, SIGNAL(mouseClicked(QPoint)), this, SLOT(on_mouseClicked(QPoint)));
+    disconnect(ui->View, SIGNAL(mouseDoubleClick(QPoint)), this, SLOT(on_mouseDoubleClick(QPoint)));
+    disconnect(ui->View, SIGNAL(keyPress(QKeyEvent*)), this, SLOT(on_keyPress(QKeyEvent*)));
 }
 
 void MainWindow::paletteInit(){
@@ -191,9 +251,14 @@ void MainWindow::on_editCheckBox_stateChanged(int arg1)
 
 void MainWindow::paintEvent(QPaintEvent *)
 {
+    return;
+    if(ui->stackedWidget->currentIndex()!=0){
+
+        return;
+    }
     QPainter painter(this);
     int W=width();
-    int pos=ui->groupBox_2->y()+ui->groupBox_2->height();
+    int pos=ui->groupBox_5->y()+ui->groupBox_5->height();
     int H=height()-pos;
     int side=qMin(W,H);
     QRect rect(0,pos+(H-side)/2,side,side);
@@ -203,7 +268,7 @@ void MainWindow::paintEvent(QPaintEvent *)
     painter.setRenderHint(QPainter::Antialiasing);
     QPen pen;
     pen.setWidth(1);
-    pen.setColor(Qt::red);
+    pen.setColor(Qt::blue);
     pen.setStyle(Qt::SolidLine);
     painter.setPen(pen);
     for(int i=0;i<36;++i){
@@ -211,3 +276,414 @@ void MainWindow::paintEvent(QPaintEvent *)
         painter.rotate(10);
     }
 }
+
+void MainWindow::onStackedWidgetPageChanged(int index){
+    if(index==1){
+        drawInit();
+    }else if(index==0){
+        resetDrawPage();
+    }
+}
+
+int MainWindow::getRand(int l,int r){
+    int len=r-l+1;
+    int res=l+ QRandomGenerator::global()->bounded(len);
+    return res;
+}
+
+void MainWindow::on_mouseMovePoint(QPoint point){
+    labViewCord->setText(QString::asprintf("View 坐标: %d %d",point.x(),point.y()));
+    QPointF pointScene=ui->View->mapToScene(point);
+    labSceneCord->setText(QString::asprintf("Secen 坐标: %.0f,%.0f",pointScene.x(),pointScene.y()));
+}
+
+void MainWindow::on_mouseClicked(QPoint point){
+    QPointF pointScene=ui->View->mapToScene(point);
+    QGraphicsItem *item=NULL;
+    item=scene->itemAt(pointScene,ui->View->transform());
+    if(item!=NULL){
+        QPointF pointItem=item->mapFromScene(pointScene);
+        labItemCord->setText(QString::asprintf("Item 坐标：%.0f,%.0f",pointItem.x(),pointItem.y()));
+        labItemInfo->setText(item->data(ItemDesciption).toString()+", ItemId="+
+                             item->data(ItemId).toString());
+    }
+}
+
+template<typename T> void setBrushColor(T *item){
+    QColor color=item->brush().color();
+    color=QColorDialog::getColor(color,NULL,"选择填充颜色");
+    if(color.isValid()){
+        item->setBrush(QBrush(color));
+    }
+}
+void MainWindow::on_mouseDoubleClick(QPoint point){
+    QPointF pointScene=ui->View->mapToScene(point);
+    QGraphicsItem  *item=NULL;
+    item=scene->itemAt(pointScene,ui->View->transform());
+    if (item == NULL){
+        return;
+    }
+    switch (item->type()){
+        case QGraphicsRectItem::Type:{
+        QGraphicsRectItem *theItem=qgraphicsitem_cast<QGraphicsRectItem*>(item);
+            setBrushColor(theItem);
+            break;
+        }
+        case QGraphicsEllipseItem::Type:{
+            QGraphicsEllipseItem *theItem;
+            theItem=qgraphicsitem_cast<QGraphicsEllipseItem*>(item);
+            setBrushColor(theItem);
+            break;
+        }
+
+        case QGraphicsPolygonItem::Type:{
+            QGraphicsPolygonItem *theItem=qgraphicsitem_cast<QGraphicsPolygonItem*>(item);
+            setBrushColor(theItem);
+            break;
+        }
+        case QGraphicsLineItem::Type:{
+            QGraphicsLineItem *theItem=qgraphicsitem_cast<QGraphicsLineItem*>(item);
+            QPen    pen=theItem->pen();
+            QColor  color=theItem->pen().color();
+            color=QColorDialog::getColor(color,this,"选择线条颜色");
+            if (color.isValid()){
+                pen.setColor(color);
+                theItem->setPen(pen);
+            }
+            break;
+        }
+        case QGraphicsTextItem::Type:{
+            QGraphicsTextItem *theItem=qgraphicsitem_cast<QGraphicsTextItem*>(item);
+            QFont font=theItem->font();
+            bool ok=false;
+            font=QFontDialog::getFont(&ok,font,this,"设置字体");
+            if (ok){
+                theItem->setFont(font);
+            }
+            break;
+        }
+    }
+}
+
+void MainWindow::on_keyPress(QKeyEvent *event){
+    if (scene->selectedItems().count()!=1){
+        return;
+    }
+    QGraphicsItem *item=scene->selectedItems().at(0);
+
+    if (event->key()==Qt::Key_Delete){
+        scene->removeItem(item);
+    }
+    else if (event->key()==Qt::Key_Space){
+        item->setRotation(90+item->rotation());
+    }
+    else if (event->key()==Qt::Key_PageUp){
+        item->setScale(0.1+item->scale());
+    }
+    else if (event->key()==Qt::Key_PageDown){
+        item->setScale(-0.1+item->scale());
+    }
+    else if (event->key()==Qt::Key_Left){
+        item->setX(-1+item->x());
+    }
+    else if (event->key()==Qt::Key_Right){
+        item->setX(1+item->x());
+    }
+    else if (event->key()==Qt::Key_Up){
+        item->setY(-1+item->y());
+    }
+    else if (event->key()==Qt::Key_Down){
+        item->setY(1+item->y());
+    }
+}
+
+void MainWindow::on_actItem_Rect_triggered()
+{
+    QGraphicsRectItem   *item=new QGraphicsRectItem(-50,-25,100,50);
+    item->setFlags(QGraphicsItem::ItemIsMovable
+                   | QGraphicsItem::ItemIsSelectable
+                   | QGraphicsItem::ItemIsFocusable);
+    item->setBrush(QBrush(Qt::yellow));
+    item->setZValue(++frontZ);
+    item->setPos(-50+(getRand(0,100)),-50+(getRand(0,100)));
+    item->setData(ItemId,++seqNum);
+    item->setData(ItemDesciption,"矩形");
+    scene->addItem(item);
+    scene->clearSelection();
+    item->setSelected(true);
+}
+
+void MainWindow::on_actItem_Ellipse_triggered()
+{
+    QRandomGenerator::global()->bounded(40001);
+    QGraphicsEllipseItem *item=new QGraphicsEllipseItem(-50,-30,100,60);
+    item->setFlags(QGraphicsItem::ItemIsMovable
+                   | QGraphicsItem::ItemIsSelectable
+                   | QGraphicsItem::ItemIsFocusable);
+    item->setBrush(QBrush(Qt::blue));
+    item->setZValue(++frontZ);
+    item->setPos(-50+(getRand(0,100)),-50+(getRand(0,100)));
+    item->setData(ItemId,++seqNum);
+    item->setData(ItemDesciption,"椭圆");
+    scene->addItem(item);
+    scene->clearSelection();
+    item->setSelected(true);
+}
+
+
+void MainWindow::on_actItem_Circle_triggered()
+{
+    QGraphicsEllipseItem *item=new QGraphicsEllipseItem(-50,-50,100,100);
+    item->setFlags(QGraphicsItem::ItemIsMovable
+                   | QGraphicsItem::ItemIsSelectable
+                   | QGraphicsItem::ItemIsFocusable);
+    item->setBrush(QBrush(Qt::cyan));
+    item->setZValue(++frontZ);
+    item->setPos(-50+(getRand(0,100)),-50+(getRand(0,100)));
+    item->setData(ItemId,++seqNum);
+    item->setData(ItemDesciption,"圆形");
+    scene->addItem(item);
+    scene->clearSelection();
+    item->setSelected(true);
+}
+
+
+void MainWindow::on_actItem_Triangle_triggered()
+{
+    QGraphicsPolygonItem *item=new QGraphicsPolygonItem;
+    QPolygonF points;
+    points.append(QPointF(0,-40));
+    points.append(QPointF(60,40));
+    points.append(QPointF(-60,40));
+    item->setPolygon(points);
+    item->setPos(-50+(getRand(0,100)),-50+(getRand(0,100)));
+    item->setFlags(QGraphicsItem::ItemIsMovable
+                   | QGraphicsItem::ItemIsSelectable
+                   | QGraphicsItem::ItemIsFocusable);
+    item->setBrush(QBrush(Qt::magenta));
+    item->setZValue(++frontZ);
+    item->setData(ItemId,++seqNum);
+    item->setData(ItemDesciption,"三角形");
+    scene->addItem(item);
+    scene->clearSelection();
+    item->setSelected(true);
+}
+
+
+void MainWindow::on_actItem_Polygon_triggered()
+{
+    QGraphicsPolygonItem *item=new QGraphicsPolygonItem;
+    QPolygonF points;
+    points.append(QPointF(-40,-40));
+    points.append(QPointF(40,-40));
+    points.append(QPointF(100,40));
+    points.append(QPointF(-100,40));
+    item->setPolygon(points);
+    item->setPos(-50+(getRand(0,100)),-50+(getRand(0,100)));
+    item->setFlags(QGraphicsItem::ItemIsMovable
+                   | QGraphicsItem::ItemIsSelectable
+                   | QGraphicsItem::ItemIsFocusable);
+    item->setBrush(QBrush(Qt::green));
+    item->setZValue(++frontZ);
+    item->setData(ItemId,++seqNum);
+    item->setData(ItemDesciption,"梯形");
+    scene->addItem(item);
+    scene->clearSelection();
+    item->setSelected(true);
+}
+
+
+void MainWindow::on_actItem_Line_triggered()
+{
+    QGraphicsLineItem *item=new QGraphicsLineItem(-100,0,100,0);
+    item->setFlags(QGraphicsItem::ItemIsMovable
+                   | QGraphicsItem::ItemIsSelectable
+                   | QGraphicsItem::ItemIsFocusable);
+    QPen pen(Qt::red);
+    pen.setWidth(3);
+    item->setPen(pen);
+    item->setZValue(++frontZ);
+    item->setPos(-50+(getRand(0,100)),-50+(getRand(0,100)));
+    item->setData(ItemId,++seqNum);
+    item->setData(ItemDesciption,"直线");
+    scene->addItem(item);
+    scene->clearSelection();
+    item->setSelected(true);
+}
+
+
+void MainWindow::on_actItem_Text_triggered()
+{
+    QString str=QInputDialog::getText(this,"输入文字","请输入文字");
+    if (str.isEmpty()){
+        return;
+    }
+    QGraphicsTextItem *item=new QGraphicsTextItem(str);
+    QFont font=this->font();
+    font.setPointSize(20);
+    font.setBold(true);
+    item->setFont(font);
+    item->setFlags(QGraphicsItem::ItemIsMovable
+                   | QGraphicsItem::ItemIsSelectable
+                   | QGraphicsItem::ItemIsFocusable);
+    item->setPos(-50+(getRand(0,100)),-50+(getRand(0,100)));
+    item->setZValue(++frontZ);
+    item->setData(ItemId,++seqNum);
+    item->setData(ItemDesciption,"文字");
+    scene->addItem(item);
+    scene->clearSelection();
+    item->setSelected(true);
+}
+
+
+void MainWindow::on_actZoomIn_triggered()
+{
+    int cnt=scene->selectedItems().count();
+    if (cnt==0){
+        ui->View->scale(1.1,1.1);
+    }
+    else{
+        for(int i=0;i<cnt;++i){
+            QGraphicsItem *item;
+            item=scene->selectedItems().at(i);
+            item->setScale(0.1+item->scale());
+        }
+    }
+}
+
+
+void MainWindow::on_actZoomOut_triggered()
+{
+    int cnt=scene->selectedItems().count();
+    if (cnt==0){
+        ui->View->scale(0.9,0.9);
+    }
+    else{
+        for(int i=0;i<cnt;++i){
+            QGraphicsItem *item;
+            item=scene->selectedItems().at(i);
+            item->setScale(item->scale()-0.1);
+        }
+    }
+}
+
+
+void MainWindow::on_actRestore_triggered()
+{
+    int cnt=scene->selectedItems().count();
+    if (cnt==0){
+        ui->View->resetTransform();
+    }
+    else{
+        for(int i=0;i<cnt;++i){
+            QGraphicsItem* item=scene->selectedItems().at(i);
+            item->setRotation(0);
+            item->setScale(1.0);
+        }
+    }
+}
+
+
+void MainWindow::on_actRotateLeft_triggered()
+{
+    int cnt=scene->selectedItems().count();
+    if (cnt==0){
+        ui->View->rotate(-30);
+    }
+    else{
+        for(int i=0;i<cnt;++i){
+            QGraphicsItem* item=scene->selectedItems().at(i);
+            item->setRotation(-30+item->rotation());
+        }
+    }
+}
+
+
+
+void MainWindow::on_actRotateRight_triggered()
+{
+    int cnt=scene->selectedItems().count();
+    if (cnt==0){
+        ui->View->rotate(+30);
+    }
+    else{
+        for(int i=0;i<cnt;++i){
+            QGraphicsItem* item=scene->selectedItems().at(i);
+            item->setRotation(+30+item->rotation());
+        }
+    }
+}
+
+
+void MainWindow::on_actEdit_Front_triggered()
+{
+    int cnt=scene->selectedItems().count();
+    if (cnt>0){
+        QGraphicsItem* item=scene->selectedItems().at(0);
+        item->setZValue(++frontZ);
+    }
+}
+
+
+void MainWindow::on_actEdit_Back_triggered()
+{
+    int cnt=scene->selectedItems().count();
+    if (cnt>0){
+        QGraphicsItem* item=scene->selectedItems().at(0);
+        item->setZValue(--backZ);
+    }
+}
+
+
+
+void MainWindow::on_actGroup_triggered()
+{
+    int cnt=scene->selectedItems().count();
+    if (cnt>1){
+        QGraphicsItemGroup* group =new QGraphicsItemGroup;
+        scene->addItem(group);
+        for (int i=0;i<cnt;i++){
+            QGraphicsItem* item=scene->selectedItems().at(0);
+            item->setSelected(false);
+            item->clearFocus();
+            group->addToGroup(item);
+        }
+        group->setFlags(QGraphicsItem::ItemIsMovable
+                        | QGraphicsItem::ItemIsSelectable
+                        | QGraphicsItem::ItemIsFocusable);
+
+        group->setZValue(++frontZ);
+        scene->clearSelection();
+        group->setSelected(true);
+    }
+}
+
+
+void MainWindow::on_actGroupBreak_triggered()
+{
+    int cnt=scene->selectedItems().count();
+    if (cnt==1){
+        QGraphicsItemGroup  *group;
+        group=(QGraphicsItemGroup*)scene->selectedItems().at(0);
+        scene->destroyItemGroup(group);
+    }
+}
+
+
+void MainWindow::on_actEdit_Delete_triggered()
+{
+    int cnt=scene->selectedItems().count();
+    if (cnt>0){
+        for (int i=0;i<cnt;i++){
+            QGraphicsItem*  item=scene->selectedItems().at(0);
+            scene->removeItem(item);
+        }
+    }
+}
+
+
+void MainWindow::on_actQuit_triggered()
+{
+    return;
+}
+
--- mainwindow.h
+++ mainwindow.h
@@ -6,6 +6,13 @@
 #include <QTimer>
 #include <QListWidgetItem>
 #include <QPainter>
+#include <QGraphicsScene>
+#include <QLabel>
+#include <QRandomGenerator>
+#include <QGraphicsEllipseItem>
+#include <QColorDialog>
+#include <QFontDialog>
+#include <QInputDialog>
 
 
 QT_BEGIN_NAMESPACE
@@ -23,23 +30,53 @@ public:
     ~MainWindow();
 
 private slots:
+    void on_mouseMovePoint(QPoint point);
+    void on_mouseClicked(QPoint point);
+    void on_mouseDoubleClick(QPoint point);
+    void on_keyPress(QKeyEvent *event);
     void on_listIniButton_clicked();
-
     void on_insertButton_clicked();
-
     void on_clearButton_clicked();
-
     void on_delButton_clicked();
-
     void on_addButton_clicked();
-
     void on_allSelectButton_clicked();
-
     void on_revSelectButton_clicked();
-
     void on_allNotSelectButton_clicked();
-
     void on_editCheckBox_stateChanged(int arg1);
+    void on_actItem_Rect_triggered();
+    void on_actItem_Ellipse_triggered();
+
+    void on_actItem_Circle_triggered();
+
+    void on_actItem_Triangle_triggered();
+
+    void on_actItem_Polygon_triggered();
+
+    void on_actItem_Line_triggered();
+
+    void on_actItem_Text_triggered();
+
+    void on_actZoomIn_triggered();
+
+    void on_actZoomOut_triggered();
+
+    void on_actRestore_triggered();
+
+    void on_actRotateLeft_triggered();
+
+    void on_actRotateRight_triggered();
+
+    void on_actEdit_Front_triggered();
+
+    void on_actEdit_Back_triggered();
+
+    void on_actGroup_triggered();
+
+    void on_actGroupBreak_triggered();
+
+    void on_actEdit_Delete_triggered();
+
+    void on_actQuit_triggered();
 
 private:
     void paletteInit();
@@ -47,10 +84,25 @@ private:
     void timerInit();
     void updateCld();
     void paintEvent(QPaintEvent *);
+    int getRand(int l,int r);
+    void drawInit();
+    void resetDrawPage();
+    void onStackedWidgetPageChanged(int index);
 private:
     Ui::MainWindow *ui;
     QTimer *my_timer;
     QDateTime last_time;
     bool time_status;
+    static const int ItemId =1;
+    static const int ItemDesciption=2;
+    int seqNum=0;
+    int frontZ=0;
+    int backZ=0;
+    QGraphicsScene *scene;
+    QLabel *labViewCord;
+    QLabel *labSceneCord;
+    QLabel *labItemCord;
+    QLabel *labItemInfo;
+
 };
 #endif // MAINWINDOW_H
